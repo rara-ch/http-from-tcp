4d67e6457b6c92ed45203643b834e50f68c0d55d
rahulc <rahulchitkara2001@gmail.com>
2025-12-28 10:53:24 +1100

    Read and print messages.txt 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Print("Init")
+	const filePath = "messages.txt"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("could not open %s for reading: %v\n", filePath, err)
+	}
+	defer file.Close()
+
+	fmt.Printf("Reading data from %s\n", filePath)
+	fmt.Println("=======================================")
+
+	for {
+		container := make([]byte, 8)
+		_, err := file.Read(container)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error other than io.EOF retured from *os.File.Read()\n")
+		}
+
+		fmt.Printf("read: %s\n", string(container))
+	}
 }
